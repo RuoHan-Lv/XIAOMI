8ea0f27c0703725e93a82d49edd469eff433c4ee
RuoHan-Lv <1521865235@qq.com>
2021-06-08 22:36:08 +0800

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,35 @@
+// 实现li背景颜色样式变化
+var tabs = document.getElementById("tabs").getElementsByTagName("li");
+// console.log(tabs);
+var lists = document.getElementById("lists").getElementsByTagName("ul");
+// console.log(lists);
+
+for(var i=0; i<tabs.length; i++){
+    tabs[i].onclick = showlist;
+
+}
+
+function showlist(){
+    for(var i=0; i<tabs.length; i++){
+        if(tabs[i] === this){
+            tabs[i].className = "active";
+            lists[i].className="clearfix active";
+        }
+        else{
+            tabs[i].className="";
+            lists[i].className="clearfix";
+        }
+    }
+}
+var seckillNav=document.getElementById("seckillNav");
+window.onscroll = function(){
+    var scrollTop = document.documentElement.scrollTop ||document.body.scrollTop ||  window.pageYOffset || 0;
+
+    if ( scrollTop >=260){
+        seckillNav.className="seckill-nav seckill-navfixed";
+    }
+    else{
+        seckillNav.className="seckill-nav";
+    }
+    console.log(scrollTop)
+}
